99cbce8064d96ff1c77d2e969bf32ed329eead15
StrongX <385135499@qq.com>
2015-04-14 22:03:21 +0800

    update

--- Memory/TabBarViewController.swift
+++ Memory/TabBarViewController.swift
@@ -14,6 +14,7 @@ class TabBarViewController: UITabBarController {
         super.viewDidLoad()
         self.tabBarController?.tabBar.selectedImageTintColor=UIColor.purpleColor()
 
+    //    var a=DTAttributedTextView()
 
     }
 
